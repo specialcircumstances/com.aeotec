98ec4d78699fb4941fbdc86542cc18c95d2b2de2
specialcircumstances <jonathanjstevens@gmail.com>
2018-05-10 22:46:44 +0100

    add measure_battery for DSB09

--- drivers/DSB09/driver.js
+++ drivers/DSB09/driver.js
@@ -12,6 +12,17 @@ const ZwaveDriver = require('homey-zwavedriver');
 module.exports = new ZwaveDriver(path.basename(__dirname), {
   debug: true,
   capabilities: {
+    measure_battery: {
+			command_class: 'COMMAND_CLASS_BATTERY',
+			command_get: 'BATTERY_GET',
+			command_report: 'BATTERY_REPORT',
+			command_report_parser: report => {
+				if (report['Battery Level'] === 'battery low warning') return 1;
+				if (report.hasOwnProperty('Battery Level (Raw)')) return report['Battery Level (Raw)'][0];
+				return null;
+			},
+			getOnWakeUp: true,
+		},
     measure_power: {
       command_class: 'COMMAND_CLASS_METER',
       command_get: 'METER_GET',
